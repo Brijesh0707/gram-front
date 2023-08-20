e94145ef06134913689dac4c94dcce8257d2a555
BRIJESH <122820661+Brijesh0707@users.noreply.github.com>
2023-08-20 13:01:11 +0530

    Change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
-// Home.js
 import React, { useEffect, useState } from 'react';
 import Logo from '../images/s-logo-1.png';
+import Loading from '../images/loading.gif';
 import './Home.css';
 import { useNavigate, Link } from 'react-router-dom';
 
 const Home = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const token = localStorage.getItem('jwt');
@@ -22,8 +23,14 @@ const Home = () => {
       },
     })
       .then((res) => res.json())
-      .then((result) => setData(result.reverse()))
-      .catch((err) => console.log(err));
+      .then((result) => {
+        setData(result.reverse());
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
   const handleLike = (postIndex) => {
@@ -67,48 +74,54 @@ const Home = () => {
 
   return (
     <div className='home-container'>
-      <div className='row'>
-        {data.map((post, index) => (
-          <div key={post._id} className='col-md-12'>
-            <div className='cards'>
-              <div className='profile-1'>
-                <div className='profile-2'>
-                  <img src={post.photo} className='logo-profile' alt='Profile' />
+      {isLoading ? (
+        <div className='loading-container'>
+          <img src={Loading} alt='Loading' className='loading-indicator' />
+        </div>
+      ) : (
+        <div className='row'>
+          {data.map((post, index) => (
+            <div key={post._id} className='col-md-12'>
+              <div className='cards'>
+                <div className='profile-1'>
+                  <div className='profile-2'>
+                    <img src={post.photo} className='logo-profile' alt='Profile' />
+                  </div>
+                  <div className='profile-name'>
+                    <Link to={`/userprofile/${post.postedby._id}`} className='userprofile'>
+                      <h6>{post.postedby.username}</h6>
+                    </Link>
+                  </div>
                 </div>
-                <div className='profile-name'>
-                  <Link to={`/userprofile/${post.postedby._id}`} className='userprofile'>
-                    <h6>{post.postedby.username}</h6>
-                  </Link>
+                <div className='post-1'>
+                  <img src={post.photo} className='post-2' alt='Post' />
                 </div>
-              </div>
-              <div className='post-1'>
-                <img src={post.photo} className='post-2' alt='Post' />
-              </div>
-              <div className='like-comment'>
-                <button
-                  onClick={() => handleLike(index)}
-                  style={{
-                    backgroundColor: post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'red' : 'white',
-                    color: post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'white' : 'black',
-                    border: '1px solid black',
-                  }}
-                >
-                  {post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'Liked' : 'Like'}
-                </button>
-                <p
-                  id='likes'
-                  style={{
-                    color: post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'black' : 'gray',
-                  }}
-                >
-                  {post.likes.length} likes
-                </p>
-                <br />
+                <div className='like-comment'>
+                  <button
+                    onClick={() => handleLike(index)}
+                    style={{
+                      backgroundColor: post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'red' : 'white',
+                      color: post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'white' : 'black',
+                      border: '1px solid black',
+                    }}
+                  >
+                    {post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'Liked' : 'Like'}
+                  </button>
+                  <p
+                    id='likes'
+                    style={{
+                      color: post.likes.includes(JSON.parse(localStorage.getItem('user'))._id) ? 'black' : 'gray',
+                    }}
+                  >
+                    {post.likes.length} likes
+                  </p>
+                  <br />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
